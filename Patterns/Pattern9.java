c234cd5bf0aec95849fa88b8db13f10ce76f7933
Bahubali Nalte <142397022+BahubaliNalte@users.noreply.github.com>
2025-01-11 10:46:58 +0530

    Create Pattern9.java

---
+++ Patterns/Pattern9.java
@@ -0,0 +1,30 @@
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n=5;
+
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n-i-1; j++){
+                System.err.print(" ");
+            }
+            for (int j=0; j<2*i+1; j++){
+                System.err.print("*");
+            }
+            for (int j=0; j<n-i-1; j++){
+                System.err.print(" ");
+            }
+            System.err.println();
+        }
+        for (int i=0; i<n; i++){
+            for (int j=0; j<i; j++){
+                System.err.print(" ");
+            }
+            for (int j=0; j<2*n-(2*i+1); j++){
+                System.err.print("*");
+            }
+            for (int j=0; j<i; j++){
+                System.err.print(" ");
+            }
+            System.err.println();
+        }
+    }
+}
