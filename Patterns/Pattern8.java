9e668addd9b09cf2c22edd74ed95160c350ace5b
Bahubali Nalte <142397022+BahubaliNalte@users.noreply.github.com>
2025-01-11 10:45:01 +0530

    Create Pattern8.java

---
+++ Patterns/Pattern8.java
@@ -0,0 +1,25 @@
+public class Pattern8 {
+    public static void main(String[] args) {
+        int n=5;
+        
+        for (int i=0; i<n; i++){
+            for (int j=0; j<i; j++){
+                System.err.print(" ");
+            }
+            for (int j=0; j<2*n-(2*i+1); j++){
+                System.err.print("*");
+            }
+            for (int j=0; j<i; j++){
+                System.err.print(" ");
+            }
+            System.err.println();
+        }
+    }
+}
+
+OUTPUT:
+*********
+ *******
+  *****
+   ***
+    *
